f295cd194b8e78718f1114da73a4ad1eabe4d458
Vo Tuan <tuanvn2512@gmail.com>
2023-06-08 16:40:55 +0700

    Admin template

--- src/main/java/com/store/controller/HomeController.java
+++ src/main/java/com/store/controller/HomeController.java
@@ -3,10 +3,27 @@ package com.store.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class HomeController {
 	@RequestMapping({"/","/home"})
 	public String home() {
 		return "/layout/home";
+	}
+	@RequestMapping(value = "/index")
+	public String index() {
+		return "/Admin/index" ;
+	}
+	@RequestMapping(value = "/Oder")
+	public String Oder() {
+		return "/Admin/Oder" ;
+	}
+	@RequestMapping(value = "/Product")
+	public String Product() {
+		return "/Admin/Product" ;
+	}
+	@RequestMapping(value = "/Users")
+	public String Users() {
+		return "/Admin/Users" ;
 	}
 }
